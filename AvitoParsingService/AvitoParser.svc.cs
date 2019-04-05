5fa779715913e47f298eff1eb9aa86437969b549
pointmoon <dendy1996@yandex.ru>
2019-04-05 17:03:43 +0300

    Переименованы стандартные классы

--- AvitoParsingService/AvitoParser.svc.cs
+++ AvitoParsingService/AvitoParser.svc.cs
@@ -8,9 +8,8 @@ using System.Text;
 
 namespace AvitoParsingService
 {
-    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде, SVC-файле и файле конфигурации.
     // ПРИМЕЧАНИЕ. Чтобы запустить клиент проверки WCF для тестирования службы, выберите элементы Service1.svc или Service1.svc.cs в обозревателе решений и начните отладку.
-    public class Service1 : IService1
+    public class AvitoParser : IAvitoParser
     {
         public string GetData(int value)
         {
--- AvitoParsingService/IAvitoParser.cs
+++ AvitoParsingService/IAvitoParser.cs
@@ -10,7 +10,7 @@ namespace AvitoParsingService
 {
     // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
     [ServiceContract]
-    public interface IService1
+    public interface IAvitoParser
     {
 
         [OperationContract]
